5293e64b1b2d8aa98204cf3f2aca5ebc64683ccb
fausto <fausto.alemao@gmail.com>
2018-03-28 19:04:38 -0300

    .

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp3c86f2eb15904f6eae8b68931b551142://c7-9rx.fausto.vibytest.exp.direct:80";
+
+}
